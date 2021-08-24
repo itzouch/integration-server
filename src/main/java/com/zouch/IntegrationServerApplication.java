7141d09ec4cec2b30ae3df1a9693d110d8d66ec7
itzouch <itzouch@163.com>
2021-08-24 21:40:46 +0800

    delete anyway.

--- src/main/java/com/zouch/IntegrationServerApplication.java
+++ src/main/java/com/zouch/IntegrationServerApplication.java
@@ -1,8 +1,11 @@
-package com.zouch.integrationserver;
+package com.zouch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author zouch
+ */
 @SpringBootApplication
 public class IntegrationServerApplication {
 
